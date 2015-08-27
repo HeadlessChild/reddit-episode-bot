cac29551c1a80dacff8da61316fa34fa096eb636
HeadlessChild <dazethelaze@gmail.com>
2015-08-28 00:31:07 +0200

    fixed reply and storage of comment ids

--- reddit-bot.py
+++ reddit-bot.py
@@ -4,11 +4,15 @@ import time
 import tvdb_api
 from configparser import ConfigParser
 
-tvshow = "Seinfeld"
+tvshow = "seinfeld"
 
 t = tvdb_api.Tvdb()
 
 r = praw.Reddit("browser-based:SeinfeldEpsisode Script:v1.0 (by /u/HeadlessChild)")
+
+#Load comment id's
+with open('comment-ids.txt') as f:
+	cache = f.read().splitlines()
 
 ### LOGIN ###
 config = ConfigParser()
@@ -28,7 +32,6 @@ pattern6 = re.compile(r"""(?:s|season)(?:\s)(?P<s>\d+)(?:.*)(?:e|x|episode|\n)(?
 pattern7 = re.compile(r"""(?:s|season)(?P<s>\d+)(?:.*)(?:e|x|episode|\n)(?:\s)(?P<ep>\d+)""", re.VERBOSE)
 
 patterns = [pattern1, pattern2, pattern3, pattern4, pattern5, pattern6, pattern7 ]
-cache = []
 
 def run_bot():
 	subreddit = r.get_subreddit("test")
@@ -37,34 +40,49 @@ def run_bot():
 		comment_text = comment.body.lower()
 		for p in patterns:
 			m = re.search(p, comment_text)
-			if m and comment.id not in cache:
+			if m and comment.id not in cache and str(comment.author) != str(username):
 				try:
 					episode_info = t[tvshow][int(m.group('s'))][int(m.group('ep'))]
-					comment.reply('Seinfeld Season '+m.group('s')+' Episode '+m.group('ep')+\
+					comment.reply('**Seinfeld: Season '+m.group('s')+' Episode '+m.group('ep')+'**'+\
 								  '\n___\n'\
-								  '\n\nEpisode Name: '+episode_info['episodename']+\
-								  '\n\nOverview: '+episode_info['overview']+\
-								  '\n\nRating: '+episode_info['rating']+\
+								  '\n\n**Episode Name:** '+episode_info['episodename']+\
+								  '\n\n**Overview:** '+episode_info['overview']+\
+								  '\n\n**Director:** '+episode_info['director']+\
+								  '\n\n**Writer(s):** '+episode_info['writer']+\
+								  '\n\n**Rating:** '+episode_info['rating']+\
 								  '\n___\n'\
-								  '^| [^Help ^me ^improve!](https://github.com/HeadlessChild/reddit-episode-bot)'\
-								  '^| [^Report ^a ^bug](https://github.com/HeadlessChild/reddit-episode-bot/issues)'\
-								  '^| [^Author](https://www.reddit.com/user/HeadlessChild/)')
+								  '^| ^Hi! ^I\'m ^a ^bot ^for ^the ^subreddit [^/r/seinfeld](https://www.reddit.com/r/seinfeld) '\
+								  '^| [^Help ^me ^improve!](https://github.com/HeadlessChild/reddit-episode-bot) '\
+								  '^| [^Report ^a ^bug](https://github.com/HeadlessChild/reddit-episode-bot/issues) '\
+								  '^| [^Author](https://www.reddit.com/user/HeadlessChild/) '\
+								  '^| ^Data ^from ^[TheTVDB](http://thetvdb.com/) ')
 					cache.append(comment.id)
+					for i in cache:
+						with open('comment-ids.txt', 'ab') as f:
+							f.write(i+"\n")
 				except praw.errors.RateLimitExceeded as error:
 					print("Rate limit exceeded, must sleep for "
 							"{} mins".format(float(error.sleep_time / 60)))
 					time.sleep(error.sleep_time)
 					episode_info = t[tvshow][int(m.group('s'))][int(m.group('ep'))]
-					comment.reply('Seinfeld Season '+m.group('s')+' Episode '+m.group('ep')+\
+					comment.reply('**Seinfeld: Season '+m.group('s')+' Episode '+m.group('ep')+'**'+\
 								  '\n___\n'+\
-								  '\n\nEpisode Name: '+episode_info['episodename']+\
-								  '\n\nOverview: '+episode_info['overview']+\
-								  '\n\nRating: '+episode_info['rating']+\
+								  '\n\n**Episode Name:** '+episode_info['episodename']+\
+								  '\n\n**Overview:** '+episode_info['overview']+\
+								  '\n\n**Director:** '+episode_info['director']+\
+								  '\n\n**Writer(s):** '+episode_info['writer']+\
+								  '\n\n**Rating:** '+episode_info['rating']+\
 								  '\n___\n'\
-								  '^| [^Help ^me ^improve!](https://github.com/HeadlessChild/reddit-episode-bot)'\
-								  '^| [^Report ^a ^bug](https://github.com/HeadlessChild/reddit-episode-bot/issues)'\
-								  '^| [^Author](https://www.reddit.com/user/HeadlessChild/)')
+								  '^| ^Hi! ^I\'m ^a ^bot ^for ^the ^subreddit [^/r/seinfeld](https://www.reddit.com/r/seinfeld) '\
+								  '^| [^Help ^me ^improve!](https://github.com/HeadlessChild/reddit-episode-bot) '\
+								  '^| [^Report ^a ^bug](https://github.com/HeadlessChild/reddit-episode-bot/issues) '\
+								  '^| [^Author](https://www.reddit.com/user/HeadlessChild/) '\
+								  '^| ^Data ^from ^[TheTVDB](http://thetvdb.com/) ')
 					cache.append(comment.id)
+					for i in cache:
+						with open('comment-ids.txt', 'ab') as f:
+							f.write(i+"\n")
+
 
 while True:
 	run_bot()
