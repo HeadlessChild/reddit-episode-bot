fed73f07f94117bbca510606af71fa4be59b8f19
HeadlessChild <dazethelaze@gmail.com>
2015-09-28 19:55:39 +0200

    fix typos

--- reddit-bot.py
+++ reddit-bot.py
@@ -45,7 +45,7 @@ pattern4 = re.compile(r"""(?:(\ss)|season)(?P<s>\d+)(?:(\se)|(\sx)|episode|\n)(?
 pattern5 = re.compile(r"""(?:(\ss)|season)(?P<s>\d+)(?:.*)(?:(\se)|(\sx)|episode|\n)(?P<ep>\d+)""", re.VERBOSE)
 pattern6 = re.compile(r"""(?:(\ss)|season)(?:\s)(?P<s>\d+)(?:.*)(?:(\se)|(\sx)|episode|\n)(?P<ep>\d+)""", re.VERBOSE)
 pattern7 = re.compile(r"""(?:s|season)(?P<s>\d+)(?:.*)(?:(\se)|(\sx)|episode|\n)(?:\s)(?P<ep>\d+)""", re.VERBOSE)
-pattern8 = re.compile(r"""(?:s|season)(?P<s>\d+)(?:e|x|episode|\n)(?P<ep>\d+)(?:.*)""", re.VERBOSE)
+pattern8 = re.compile(r"""(?:s|season)(?P<s>\d+)(?:e|x|episode|\n)(?P<ep>\d+)""", re.VERBOSE)
 pattern9 = re.compile(r"""(?:s|season)(?:\s)(?P<s>\d+)(?:e|x|episode|\n)(?:\s)(?P<ep>\d+)""", re.VERBOSE)
 pattern10 = re.compile(r"""(?:s|season)(?P<s>\d+)(?:e|x|episode|\n)(?:\s)(?P<ep>\d+)""", re.VERBOSE)
 pattern11 = re.compile(r"""(?:s|season)(?:\s)(?P<s>\d+)(?:e|x|episode|\n)(?P<ep>\d+)""", re.VERBOSE)
@@ -53,17 +53,17 @@ pattern11 = re.compile(r"""(?:s|season)(?:\s)(?P<s>\d+)(?:e|x|episode|\n)(?P<ep>
 pattern12 = re.compile(r"""(?:(\se)|x|episode|\n)(?:\s)(?P<ep>\d+)(?:.*)(?:(\ss)|season)(?:\s)(?P<s>\d+)""", re.VERBOSE)
 pattern13 = re.compile(r"""(?:(\se)|x|episode|\n)(?P<ep>\d+)(?:(\ss)|season)(?:\s)(?P<s>\d+)""", re.VERBOSE)
 pattern14 = re.compile(r"""(?:(\se)|x|episode|\n)(?:\s)(?P<ep>\d+)(?:(\ss)|season)(?P<s>\d+)""", re.VERBOSE)
-pattern16 = re.compile(r"""(?:(\se)|x|episode|\n)(?P<ep>\d+)(?:(\ss)|season)(?P<s>\d+)""", re.VERBOSE)
-pattern17 = re.compile(r"""(?:(\se)|x|episode|\n)(?P<ep>\d+)(?:.*)(?:(\ss)|season)(?P<s>\d+)""", re.VERBOSE)
-pattern18 = re.compile(r"""(?:(\se)|x|episode|\n)(?:\s)(?P<ep>\d+)(?:.*)(?:(\ss)|season)(?P<s>\d+)""", re.VERBOSE)
-pattern19 = re.compile(r"""(?:(\se)|x|episode|\n)(?P<ep>\d+)(?:.*)(?:s|season)(?:\s)(?P<s>\d+)""", re.VERBOSE)
-pattern20 = re.compile(r"""(?:e|x|episode|\n)(?P<ep>\d+)(?:s|season)(?P<s>\d+)""", re.VERBOSE)
-pattern21 = re.compile(r"""?:e|x|episode|\n)(?:\s)(?P<ep>\d+)(?:s|season)(?:\s)(?P<s>\d+)""", re.VERBOSE)
-pattern22 = re.compile(r"""(?:e|x|episode|\n)(?P<ep>\d+)(?:s|season)(?:\s)(?P<s>\d+)""", re.VERBOSE)
-pattern23 = re.compile(r"""(?:e|x|episode|\n)(?:\s)(?P<ep>\d+)(?:s|season)(?P<s>\d+)""", re.VERBOSE)
+pattern15 = re.compile(r"""(?:(\se)|x|episode|\n)(?P<ep>\d+)(?:(\ss)|season)(?P<s>\d+)""", re.VERBOSE)
+pattern16 = re.compile(r"""(?:(\se)|x|episode|\n)(?P<ep>\d+)(?:.*)(?:(\ss)|season)(?P<s>\d+)""", re.VERBOSE)
+pattern17 = re.compile(r"""(?:(\se)|x|episode|\n)(?:\s)(?P<ep>\d+)(?:.*)(?:(\ss)|season)(?P<s>\d+)""", re.VERBOSE)
+pattern18 = re.compile(r"""(?:(\se)|x|episode|\n)(?P<ep>\d+)(?:.*)(?:s|season)(?:\s)(?P<s>\d+)""", re.VERBOSE)
+pattern19 = re.compile(r"""(?:e|x|episode|\n)(?P<ep>\d+)(?:s|season)(?P<s>\d+)""", re.VERBOSE)
+pattern20 = re.compile(r"""(?:e|x|episode|\n)(?:\s)(?P<ep>\d+)(?:s|season)(?:\s)(?P<s>\d+)""", re.VERBOSE)
+pattern21 = re.compile(r"""(?:e|x|episode|\n)(?P<ep>\d+)(?:s|season)(?:\s)(?P<s>\d+)""", re.VERBOSE)
+pattern22 = re.compile(r"""(?:e|x|episode|\n)(?:\s)(?P<ep>\d+)(?:s|season)(?P<s>\d+)""", re.VERBOSE)
 
 patterns = [pattern1, pattern2, pattern3, pattern4, pattern5, pattern6, pattern7, pattern8, pattern9, pattern10, pattern11, pattern12,
-pattern13, pattern14, pattern15, pattern16, pattern17, pattern18, pattern19, pattern20, pattern21, pattern22, pattern23 ]
+pattern13, pattern14, pattern15, pattern16, pattern17, pattern18, pattern19, pattern20, pattern21, pattern22 ]
 
 def run_bot():
 	subreddit = r.get_subreddit("seinfeld")
